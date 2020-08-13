d8741cbddbd92f6e1ea7411c068d04ab4e5eeb72
poa <Andreas.poetscher@htl-leoben.at>
2020-08-13 13:08:03 +0200

    Backup Works. Action Map DB Table is exported to actionmap.xml

--- backup/moodle2/backup_actionmap_activity_task.class.php
+++ backup/moodle2/backup_actionmap_activity_task.class.php
@@ -14,14 +14,14 @@ class backup_actionmap_activity_task extends backup_activity_task {
      * Define (add) particular settings this activity can have
      */
     protected function define_my_settings() {
-        // No particular settings for this activity
     }
  
     /**
      * Define (add) particular steps this activity can have
      */
     protected function define_my_steps() {
-        // Choice only has one structure step
+        $this->add_step(new backup_actionmap_activity_structure_step('actionmap_structure', 'actionmap.xml'));
+        // Actionmap only has one structure step
     }
  
     /**
--- backup/moodle2/backup_actionmap_stepslib.php
+++ backup/moodle2/backup_actionmap_stepslib.php
@@ -3,3 +3,31 @@
 /**
  * Define all the backup steps that will be used by the backup_choice_activity_task
  */
+class backup_actionmap_activity_structure_step extends backup_activity_structure_step {
+ 
+    protected function define_structure() {
+ 
+        // To know if we are including userinfo
+        $userinfo = $this->get_setting_value('userinfo');
+ 
+        // Define each element separated
+        $actionmap = new backup_nested_element('actionmap', array('id'), array(
+            'name', 'intro', 'introformat', 'graphdirection',
+            'content', 'nodeseperation', 'sectionbackgroundcolor', 'edgestyle',
+            'elementshape'));
+
+        // Build the tree
+        // No Tree needed. We have only one db table
+
+        // Define sources
+        $actionmap->set_source_table('actionmap', array('id' => backup::VAR_ACTIVITYID));
+  
+        // Define id annotations
+ 
+        // Define file annotations
+        $actionmap->annotate_files('mod_actionmap', 'intro', null, $contextid = null); // This file area does not have an itemid.
+
+        // Return the root element (actionmao), wrapped into standard activity structure
+        return $this->prepare_activity_structure($actionmap);
+    }
+}
