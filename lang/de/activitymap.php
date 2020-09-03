a8f468d9ae67199af33300c24962052618cfeb46
Günther Hutter <guenther.hutter@bytebang.at>
2020-09-03 10:23:30 +0200

    Added german translation

---
+++ lang/de/activitymap.php
@@ -0,0 +1,97 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Activitymap english language pack
+ *
+ * @package    mod_activitymap
+ * @copyright  2020 Günther Hutter, Andreas Poetscher
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['activitymapsettings'] = 'Activitymap Einstellungen';
+$string['graphdirection'] = 'Graph richtung';
+$string['LR'] = 'Links -> Rechts';
+$string['RL'] = 'Rechts -> Links';
+$string['TB'] = 'Oben -> Unten';
+$string['BT'] = 'Unten -> Oben';
+
+$string['edgestyle'] = 'Kantenstil';
+$string['spline'] = 'Spline';
+$string['ortho'] = 'Rechteckig';
+$string['curved'] = 'Kurvig';
+$string['polyline'] = 'Abgeknickt';
+$string['line'] = 'Gerade';
+$string['none'] = 'Keine';
+
+$string['aliceblue'] = 'Hellblau';
+$string['transparent'] = 'Transparent';
+$string['ghostwhite'] = 'Weiss';
+$string['beige'] = 'Beige';
+$string['lightgray'] = 'Hellgrau';
+$string['lightpink'] = 'Pink';
+$string['lightyellow'] = 'Gelb';
+$string['palegreen'] = 'Grün';
+$string['random'] = 'Zufällig';
+$string['sectionbackgroundcolor'] = 'Hintergrundfarbe der Abschnitte';
+
+$string['box'] = 'Viereck';
+$string['hexagon'] = 'Sechseck';
+$string['ellipse'] = 'Ellipse';
+$string['diamond'] = 'Raute';
+$string['parallelogram'] = 'Parallelogramm';
+$string['star'] = 'Stern';
+$string['note'] = 'Notiz';
+$string['tab'] = 'Registerkarte';
+$string['folder'] = 'Ordner';
+$string['elementshape'] = 'Form der Knoten';
+
+$string['nodeseperation'] = 'Abstand zwischen Knoten';
+
+$string['allSectionsGrouped'] = 'Alle Aktivitäten, gruppiert nach Abschnitten';
+$string['allSections'] = 'Alle Aktivitäten';
+$string['currentSection'] = 'Nur Aktivitäten des aktuellen Abschnitts';
+
+$string['content'] = 'Inhalt';
+
+$string['condition_AND_label'] = ' & ';
+$string['condition_AND_tooltip'] = 'Alle Vorgängeraktivitäten müssen erfüllt sein';
+$string['condition_OR_label'] = '>=1';
+$string['condition_OR_tooltip'] = 'Zumindest eine Vorgängeraktivität müssen erfüllt sein';
+
+$string['activity_from_other_section'] = 'Diese aktivität stammt aus einem anderen Abschnitt';
+$string['not_existing_activity'] = 'Nicht existierende Aktivität';
+
+$string['this_is'] = 'Das sind';
+$string['days_from_now'] = 'Tage ab heute';
+$string['before'] = 'vor';
+$string['after'] = 'nach';
+
+$string['modulename'] = 'Activitymap';
+$string['modulename_help'] = 'Eine Activitymap stellt Aktivitäten und deren Abhängigkeiten auf einer Karte dar. Es ist ein guter Ausgangspunkt für speilerisches lernen.';
+$string['modulename_link'] = 'mod/activitymap/view';
+$string['modulenameplural'] = 'Activitymaps';
+$string['page-mod-url-x'] = 'Eine Activitymap modulseite';
+$string['pluginadministration'] = 'Activitymap module Administration';
+$string['pluginname'] = 'Activitymap';
+$string['printintro'] = 'Beschreibung zu Activitymap anzeigen';
+$string['printintroexplain'] = 'Display description below content? Some display types may not display description even if enabled.';
+$string['privacy:metadata'] = 'Das Activitymap plugin speichert keine persönlichen Daten.';
+
+$string['activitymap:addinstance'] = 'Eine Activitymap hinzufügen';
+$string['activitymap:view'] = 'Activitymap öffnen';
+
